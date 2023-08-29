acb1af385e9285b44f1d367c8d3bf7001399d73d
tonan <tonantruong@hotmail.com>
2023-08-29 10:18:06 -0400

    completed National Parks

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,40 +1,95 @@
 class NationalPark:
+    all = []
 
     def __init__(self, name):
-        self.name = name
-        
+        if type(name) != str:
+            raise Exception
+        if len(name) <= 3:
+            raise Exception
+        self._name = name
+        NationalPark.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
     def trips(self):
-        pass
-    
+        return [trip for trip in Trip.all if trip.national_park == self]
+
     def visitors(self):
-        pass
-    
+        return list(set(trip.visitor for trip in self.trips()))
+
     def total_visits(self):
-        pass
-    
+        total = 0
+        for trip in self.trips():
+            total += 1
+        return total
+
     def best_visitor(self):
-        pass
+        vistors = []
+        for trip in self.trips():
+            vistors.append(trip.visitor)
+        return max(vistors, key=vistors.count)
 
 
 class Trip:
-    
+    all = []
+
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
-        self.start_date = start_date
-        self.end_date = end_date
+        if not isinstance(start_date, str):
+            raise Exception
+        self._start_date = start_date
+        if type(end_date) != str:
+            raise Exception
+        self._end_date = end_date
+        Trip.all.append(self)
+    
+    @property
+    def start_date(self):
+        return self._start_date
+
+    @start_date.setter
+    def start_date(self, value):
+        self._start_date = value
+    
+    @property
+    def end_date(self):
+        return self._end_date
+    
+    @end_date.setter
+    def end_date(self, value):
+        self._end_date = value
 
 
 class Visitor:
+    all = []
 
     def __init__(self, name):
+        if type(name) != str:
+            raise Exception
+        if len(name) > 15 or len(name) < 1:
+            raise Exception
         self.name = name
-        
+        Visitor.all.append(self)
+
     def trips(self):
-        pass
-    
+        return [trip for trip in Trip.all if trip.visitor == self]
+
     def national_parks(self):
-        pass
-    
+        return list(set(trip.national_park for trip in self.trips()))
+
     def total_visits_at_park(self, park):
-        pass
+        total = 0
+        for trip in self.trips():
+            if trip.national_park == park:
+                total += 1
+        return total
+
+
+
+
+
+
+
